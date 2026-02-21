5db88426c3cdbd4901c17dee6799c4854b043277
Lucas Aquino <lucas.santos@gruporbs.com.br>
2026-02-21 12:41:52 -0300

    fix(frontend): add ts-expect-error to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // @ts-expect-error: vitest types are not integrated in UserConfigExport here
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
   },
-}) as any
+})
